831ed86f0f7951d28017cbce61407a4b04d91ba8
SadovNick <alecos100@gmail.com>
2023-08-08 15:23:58 +0300

    Добавление логирования

--- src/main/java/by/sadovnick/userservice/service/UserServiceImpl.java
+++ src/main/java/by/sadovnick/userservice/service/UserServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,16 +21,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to database", user.getName());
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to database", role.getName());
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = userRepository.findByUsername(username);
         Role role = roleRepository.findByName(roleName);
         //т.к метод транзакционный то нет нужды что-либо возвращать. Все сохранится в бд по завершении транзакции.
@@ -40,11 +42,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepository.findAll();
     }
 }
